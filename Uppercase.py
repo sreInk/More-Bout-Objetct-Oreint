3ce1db661d3686f2ff02b8c6775c886e0c0be087
sreInk <srinknok@gmail.com>
2024-11-06 12:52:33 +0530

    first commit

---
+++ Uppercase.py
@@ -0,0 +1,14 @@
+class IOS():
+    def __init__(self) :
+        self.str1 = ""
+    
+    def String12(self):
+        self.str1 = input("Before FBI Destorys The World Of sqAure Write Something")
+
+    def prnter(self):
+        print("This I s not Your Report Card ",self.str1.upper())
+
+str1 = IOS()
+str1.String12()
+str1.prnter()
+    
