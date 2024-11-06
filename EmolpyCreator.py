35b78b1caee4ef9b7a65c1ff5494eed9a8e83c0b
sreInk <srinknok@gmail.com>
2024-11-06 13:01:09 +0530

    hhu

---
+++ EmolpyCreator.py
@@ -0,0 +1,15 @@
+class Employ:
+    def __init__(self):
+        print("Emolpy Made")
+    def __def__(self):
+        print("Destructior Kratos")
+def Create():
+    print("Making Employ")
+    ob = Employ()
+    print("Wait....")
+    del ob
+
+print("Calling FbI")
+ob = Create
+print("Program End")
+    
